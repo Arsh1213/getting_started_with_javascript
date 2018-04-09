56d451ff169eed90c7163201ee6b00df83a8e942
Souvik <lpmb-bang-souvik@Souviks-MacBook-Pro.local>
2018-04-09 17:59:50 +0530

    decisonmakingifelse

---
+++ Snippets/random_stuff.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+// An object that captures information relevant to the decision.
+// Any change in the values of the object's properties will affect the decision.
+var infoMatrix = {
+  programmingAptitude : true,
+  interestInWeb: true
+};
+
+// A logic bundle that helps us arrive at a good decision.
+function should_i_learn_js(infoMatrix) {
+  // The initial decision on whether to learn JavaScript or not!
+  var focusOnJavaScript = null;
+  // The following if-else block determines whether you should learn JavaScript 
+  if ((infoMatrix.programmingAptitude == true) && (infoMatrix.interestInWeb == true)) {
+    focusOnJavaScript = "Yes";
+  }
+  else {
+    focusOnJavaScript = "No";
+  }
+  return focusOnJavaScript;
+};
+
+
+
+// Call the function
+make_decision(infoMatrix);
+
+
